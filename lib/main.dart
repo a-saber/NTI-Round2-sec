f848a6c9407ed826921f90777f5521040189b9d2
a-saber <ahs629862@gmail.com>
2025-04-17 19:09:14 +0200

    sec 17/4 thu

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,258 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+class CountryModel
+{
+  String name;
+  String code;
+  CountryModel({required this.name,required this.code});
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  String toString() {
+    return "${this.name} ${this.code}";
   }
+}
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController nameController = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  final emailRegex = RegExp(
+    r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$",
+  );
+  String name='';
+  bool check = false;
+  List<CountryModel> countries = [
+    CountryModel(name: 'Egypt', code: 'eg'),
+    CountryModel(name: 'Syria', code: 'sy'),
+    CountryModel(name: 'Jordan', code: 'jo'),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        leading: Icon(Icons.add),
+        title: Text(
+          'My Home Page',
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsetsDirectional.only(end: 20),
+            child: Icon(Icons.access_alarms_sharp),
+          ),
+          Padding(
+            padding: const EdgeInsetsDirectional.only(end: 20),
+            child: Icon(Icons.access_alarms_sharp),
+          ),
+        ],
+      ),
+
+      body: Form(
+        key: formKey,
+        child: Column(
+          children:
+          [
+
+            Switch(
+              activeColor: Colors.amberAccent,
+                inactiveThumbColor: Colors.blue,
+                inactiveTrackColor: Colors.black,
+                value: check,
+                onChanged: (bool? value){
+              setState(() {
+                if(value != null) check = value;
+              });
+            }),
+            Checkbox(
+              checkColor: Colors.blue,
+                fillColor: WidgetStateProperty.all(Colors.red),
+                value: check,
+                onChanged: (bool? value){
+              setState(() {
+                if(value != null) check = value;
+              });
+            }),
+
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextFormField(
+                autocorrect: true,
+                controller: nameController,
+                enabled: true,
+                onTap: ()
+                {
+                  // showTimePicker(context: context,
+                  //     initialTime: TimeOfDay.now());
+                  // showDatePicker(context: context,
+                  //   firstDate: DateTime.now(),
+                  //     lastDate: DateTime(2025, )
+                  // );
+                  // print('Form tapped');
+                },
+                onChanged: (String value)
+                {
+                  // if(!formKey.currentState!.validate()) return;
+                  //
+                  // setState(() {
+                  //   name = nameController.text;
+                  // });
+                },
+                validator: (String? value)
+                {
+                  if(value == null || value.isEmpty)
+                  {
+                    return "Filed is required";
+                  }
+                  if (!emailRegex.hasMatch(value))
+                  {
+                    return 'email not valid';
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: "Enter Your Name",
+                  labelText: "Name",
+                  prefixIcon: Icon(Icons.person),
+                  suffixIcon: Icon(Icons.person),
+                  suffixIconColor: Colors.amberAccent,
+                  enabledBorder: AppBorderDecoration.formFieldDecoration(Colors.grey),
+                  disabledBorder: AppBorderDecoration.formFieldDecoration(Colors.grey.withAlpha(50)),
+                  focusedBorder: AppBorderDecoration.formFieldDecoration(Colors.blue),
+                  errorBorder: AppBorderDecoration.formFieldDecoration(Colors.red),
+                  focusedErrorBorder: AppBorderDecoration.formFieldDecoration(Colors.blue),
+                  // border: OutlineInputBorder(
+                  //   borderRadius: BorderRadius.circular(20),
+                  //   borderSide: BorderSide(
+                  //     color: Colors.red
+                  //   )
+                  // )
+                ),
+              ),
+            ),
+          DropdownButtonFormField(
+              decoration: InputDecoration(
+                hintText: "Enter Your Name",
+                labelText: "Name",
+                prefixIcon: Icon(Icons.person),
+                suffixIcon: Icon(Icons.person),
+                suffixIconColor: Colors.amberAccent,
+                enabledBorder:
+                    AppBorderDecoration.formFieldDecoration(Colors.grey),
+                disabledBorder: AppBorderDecoration.formFieldDecoration(
+                    Colors.grey.withAlpha(50)),
+                focusedBorder:
+                    AppBorderDecoration.formFieldDecoration(Colors.blue),
+                errorBorder:
+                    AppBorderDecoration.formFieldDecoration(Colors.red),
+                focusedErrorBorder:
+                    AppBorderDecoration.formFieldDecoration(Colors.blue),
+                // border: OutlineInputBorder(
+                //   borderRadius: BorderRadius.circular(20),
+                //   borderSide: BorderSide(
+                //     color: Colors.red
+                //   )
+                // )
+              ),
+              items: countries.map(
+                  (item) => DropdownMenuItem(value: item, child:
+                  Text(item.toString()))
+              ).toList(),
+              onChanged: (value) {
+                print(value?.name);
+                print(value?.code);
+              }),
+          SizedBox(height: 50,),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+                elevation: 20,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  side: BorderSide(color: Colors.black, width: 5)
+                )
+              ),
+              onPressed: ()
+              {
+                if(!formKey.currentState!.validate()) return;
+
+                setState(() {
+                  name = nameController.text;
+                });
+              },
+              child: Text('Validate')
+            ),
+            SizedBox(height: 50,),
+            Text(name),
+          ]),
+      ),
+    );
+  }
+}
+abstract class AppBorderDecoration
+{
+  static InputBorder formFieldDecoration (Color color)
+  {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20),
+        borderSide: BorderSide(
+            color: color
+        )
+    );
+  }
+
+}
+
+class RowBuilder extends StatelessWidget {
+  const RowBuilder({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15.0),
+      child: Row(
+        children: [
+          for (int i = 0; i < 3; i++)
+            Builder(builder: (context) {
+              if (i == 0) {
+                return ContainerItemBuilder();
+              } else {
+                return SizedBox();
+              }
+            })
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ContainerItemBuilder extends StatelessWidget {
+  const ContainerItemBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: 60,
+      color: Colors.grey,
+      margin: EdgeInsets.symmetric(horizontal: 10),
     );
   }
 }
